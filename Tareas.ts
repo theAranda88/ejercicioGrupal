f971f751c2ae20fc6524f564fadbf30b4df899d7
Daniel Aranda <thearanda88@gmail.com>
2024-11-08 19:23:38 -0500

    :fire: My second commit base modificado :fire:

--- Tareas.ts
+++ Tareas.ts
@@ -7,24 +7,20 @@ type  Tarea = {
 let tareas: Tarea[] = [
     { id: 1, nombre: 'Tarea 1', completada: false },
     { id: 2, nombre: 'Tarea 2', completada: false },
-    { id: 3, nombre: 'Tarea 3', completada: false },   // creamos una lista de tareas iniciales 
+    { id: 3, nombre: 'Tarea 3', completada: false },   // cree una lista de tareas iniciales 
     { id: 4, nombre: 'Tarea 4', completada: false },
     { id: 5, nombre: 'Tarea 5', completada: false },
 ];
 
 const cambiarEstadotarea = (tareas: Tarea[], id: number): Tarea[] => {
-    return tareas.map(tarea =>                                                //mas recorremos tareas, dentro si true la condicional spread operator
-        tarea.id === id? {...tarea, completada:!tarea.completada } : tarea    //para copiar tarea y cambia el valor de completada al valor opuesto
-    );                                                                        //muestra el estado usando el operador ternario
+    return tareas.map(tarea =>                                                //map con el q recorro el array tareas
+        tarea.id === id? {...tarea, completada:!tarea.completada } : tarea    //verificamos por el condicional ternario si tarea.id es igual a id
+    );                                                                        //si es asi creamos copia del array cambiandole el valor a completada sino devuelve tarea sin modificacion
 }
 
 const obtenerEstadoTartea = (tarea: Tarea): string =>           //funcion flecha
     tarea.completada? 'Completada' : 'Pendiente';               //usamos operador ternario para evaluar si es true o false y decir si es
                                                                 //completada o pendiente
-
-const nuevaTarea: Tarea =  { id: 6, nombre: 'Tarea 6', completada: false }; 
-const tareasActualizadas = [...tareas, nuevaTarea];
-
 
 
 // angi = mostrar resultados
